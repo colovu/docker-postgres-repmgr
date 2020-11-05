2852cb46d68f1905e5f4ae6f85b3739f4d761962
Endial Fang <endial@126.com>
2020-11-05 09:11:48 +0800

    [fix:10]增加事件响应脚本

--- customer/usr/local/repmgr/events/execs/primary_unregister.sh
+++ customer/usr/local/repmgr/events/execs/primary_unregister.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# 主库失效时，在新的主库注册当前节点
+# 取消当前节点的主库注册
 
 # 设置 shell 执行参数，可使用'-'(打开）'+'（关闭）控制。常用：
 # 	-e: 命令执行错误则报错; -u: 变量未定义则报错; -x: 打印实际待执行的命令行; -o pipefail: 设置管道中命令遇到失败则报错
@@ -8,5 +8,5 @@ set -eu
 set -o pipefail
 
 . "${REPMGR_EVENTS_DIR}/execs/includes/anotate_event_processing.sh"
-. "${REPMGR_EVENTS_DIR}/execs/includes/lock_standby.sh"
 . "${REPMGR_EVENTS_DIR}/execs/includes/unlock_primary.sh"
+#. "${REPMGR_EVENTS_DIR}/execs/includes/lock_standby.sh"
